950f086fac1c6a95a4f06517426898c9308764a1
NamanV19 <namanvohra19@gmail.com>
2018-10-10 21:14:12 +0700

    Alien Invasion Complete

--- Pygame.py
+++ Pygame.py
@@ -4,6 +4,10 @@ import sys
 import gamefunctions as gf
 from settings import Settings
 from ship import Ship
+from alien import Alien
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 
 
 def run_game():
@@ -12,21 +16,30 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    alien = Alien(ai_settings, screen)
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets )
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+            for bullet in bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    bullets.remove(bullet)
+        # print(len(bullets))
 
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        print(len(bullets))
 
-        gf.update_screen(ai_settings, screen, ship, bullets)
+
 run_game()
--- settings.py
+++ settings.py
@@ -1,13 +1,39 @@
 class Settings():
  """A class to store all settings for Alien Invasion."""
  def __init__(self):
-    self.screen_width = 1100
-    self.screen_height = 650
-    self.bg_color = (0, 255, 0)
+    self.screen_width = 1000
+    self.screen_height = 600
+    self.bg_color = (255, 255, 255)
     self.ship_speed_factor = 1.5
+    self.ship_limit = 3
+    self.score_scale = 1.5
 
-    self.bullet_speed_factor = 1
+    self.bullet_speed_factor = 3
     self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = 60, 60, 60
     self.bullets_allowed = 3
+
+    self.alien_speed_factor = 1
+    self.fleet_drop_speed = 10
+    self.fleet_direction = 1
+
+    self.speedup_scale = 1.1
+    self.initialize_dynamic_settings()
+
+ def initialize_dynamic_settings(self):
+    """Initialize settings that change throughout the game."""
+    self.ship_speed_factor = 1.5
+    self.bullet_speed_factor = 3
+    self.alien_speed_factor = 1
+    self.fleet_direction = 1
+    self.alien_points = 50
+
+ def increase_speed(self):
+    """Increase speed settings."""
+    self.ship_speed_factor *= self.speedup_scale
+    self.bullet_speed_factor *= self.speedup_scale
+    self.alien_speed_factor *= self.speedup_scale
+
+    self.alien_points = int(self.alien_points * self.score_scale)
+    print(self.alien_points)
